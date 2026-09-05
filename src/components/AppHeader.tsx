a27c66c087c76d7fc8d8a6758e393f78bccb2ce0
Anugerah Cahaya Utama <89703395+cahayoyo@users.noreply.github.com>
2026-09-05 13:06:14 +0700

    [CuratedByDer][HOTFIX][Fix][Admin][FE] Fix Navbar Broken at Tablet Width (#169)
    * fix: switch to mobile nav below 1024px to avoid navbar squeeze
    
    * fix: push profile menu right when desktop nav is hidden

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -42,7 +42,9 @@ export function AppHeader({
           </div>
         </div>
 
-        <div className="hidden md:flex md:flex-1 md:justify-center">{desktopNav}</div>
+        <div className="hidden xl:flex xl:flex-1 xl:justify-center">{desktopNav}</div>
+
+        <div className="hidden flex-1 md:block xl:hidden" />
 
         {menus}
       </div>
