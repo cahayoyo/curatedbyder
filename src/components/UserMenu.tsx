53821de36d4376b67be11f8d6be87b3c326d0e5e
cahayoyo <cahayoyo@gmail.com>
2026-09-11 23:31:54 +0700

    feat: add settings shortcut to admin overview header

--- src/components/UserMenu.tsx
+++ src/components/UserMenu.tsx
@@ -79,7 +79,7 @@ export function UserMenu({
           >
             <Link href="/admin/settings">
               <Settings className="h-4 w-4" />
-              Setting
+              Pengaturan
             </Link>
           </DropdownMenuItem>
         )}
